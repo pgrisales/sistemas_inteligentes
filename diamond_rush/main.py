0be595c66e320b82098a104d8ae53bc6e97d26fb
batman <pgrisales@unal.edu.co>
2022-10-08 12:01:49 -0500

    cleaning and setup main

--- diamond_rush/main.py
+++ diamond_rush/main.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 from enviroment import levels
 from enviroment import matchLevel 
-from test import t
 
 # PONER EL PATH DE LA IMG DE PRUEBA
 test_img = './test_img.png'
